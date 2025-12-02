02f403d32ac4034f5c6d7aa6d1bbb6d9e9f1eca4
Pedro Henrique Freitas Silva <pedrohenriksk8@gmail.com>
2025-12-02 20:11:43 -0300

    fundamentos0.1

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -3,8 +3,13 @@ import http from 'node:http'
 //criar um usuario(nome,email,senha)
 
 const server = http.createServer((req, res) => {
+    const method = req.method
+    const url = req.url
+
+    console.log(method, url)
+
     return res.end('Hello ignite!')
 
 })
 
-server.listen(333)
+server.listen(3333)
